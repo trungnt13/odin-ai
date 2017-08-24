76fe0402250cd86d91c684625a3f0f2c8f32b883
TrungNT <nickartin13@gmail.com>
2017-08-24 11:03:42 +0300

    Add adapted image processing code from keras

--- odin/preprocessing/image.py
+++ odin/preprocessing/image.py
@@ -1,9 +1,175 @@
+"""Fairly basic set of tools for real-time data augmentation on image data.
+Can easily be extended to include new transformations,
+new preprocessing methods, etc...
+LICENSE: https://github.com/fchollet/keras/blob/master/LICENSE
+Original code: https://github.com/fchollet/keras/blob/master/keras/preprocessing/image.py
+"""
 from __future__ import print_function, division, absolute_import
 
 import numpy as np
+import scipy.ndimage as ndi
+
 from PIL import Image
 
 from odin.utils import as_tuple
+
+
+def apply_transform(x,
+                    transform_matrix,
+                    fill_mode='nearest'):
+    """Apply the image transformation specified by a matrix.
+
+    # Arguments
+        x: 2D numpy array, single image.
+        transform_matrix: Numpy array specifying the geometric transformation.
+        channel_axis: Index of axis for channels in the input tensor.
+        fill_mode: Points outside the boundaries of the input
+            are filled according to the given mode
+            (one of `{'constant', 'nearest', 'reflect', 'wrap'}`).
+        cval: Value used for points outside the boundaries
+            of the input if `mode='constant'`.
+
+    # Returns
+        The transformed version of the input.
+    """
+    x = np.rollaxis(x, 2, 0)
+    final_affine_matrix = transform_matrix[:2, :2]
+    final_offset = transform_matrix[:2, 2]
+    channel_images = [ndi.interpolation.affine_transform(
+        x_channel,
+        final_affine_matrix,
+        final_offset,
+        order=0,
+        mode=fill_mode,
+        cval=0.) for x_channel in x]
+    x = np.stack(channel_images, axis=0)
+    x = np.rollaxis(x, 0, 2 + 1)
+    return x
+
+
+def transform_matrix_offset_center(matrix, x, y):
+    o_x = float(x) / 2 + 0.5
+    o_y = float(y) / 2 + 0.5
+    offset_matrix = np.array([[1, 0, o_x], [0, 1, o_y], [0, 0, 1]])
+    reset_matrix = np.array([[1, 0, -o_x], [0, 1, -o_y], [0, 0, 1]])
+    transform_matrix = np.dot(np.dot(offset_matrix, matrix), reset_matrix)
+    return transform_matrix
+
+
+def rotate(x, rg, fill_mode='nearest'):
+    """Performs a random rotation of a Numpy image tensor.
+
+    # Arguments
+        x: Input tensor. Must be 3D.
+        rg: Rotation range, in degrees.
+            can be negative or positive
+        fill_mode: Points outside the boundaries of the input
+            are filled according to the given mode
+            (one of `{'constant', 'nearest', 'reflect', 'wrap'}`).
+
+    # Returns
+        Rotated Numpy image tensor.
+    """
+    theta = np.pi / 180 * rg
+    rotation_matrix = np.array([[np.cos(theta), -np.sin(theta), 0],
+                                [np.sin(theta), np.cos(theta), 0],
+                                [0, 0, 1]])
+
+    h, w = x.shape[0], x.shape[1]
+    transform_matrix = transform_matrix_offset_center(rotation_matrix, h, w)
+    x = apply_transform(x, transform_matrix, fill_mode)
+    return x
+
+
+def shift(x, wrg, hrg, fill_mode='nearest'):
+    """Performs a random spatial shift of a Numpy image tensor.
+
+    # Arguments
+        x: Input tensor. Must be 3D.
+        wrg: Width shift range, as a float fraction of the width.
+            can be negative or positive
+        hrg: Height shift range, as a float fraction of the height.
+            can be negative or positive
+        fill_mode: Points outside the boundaries of the input
+            are filled according to the given mode
+            (one of `{'constant', 'nearest', 'reflect', 'wrap'}`).
+
+    # Returns
+        Shifted Numpy image tensor.
+    """
+    h, w = x.shape[0], x.shape[1]
+    tx = hrg * h
+    ty = wrg * w
+    translation_matrix = np.array([[1, 0, tx],
+                                   [0, 1, ty],
+                                   [0, 0, 1]])
+
+    transform_matrix = translation_matrix  # no need to do offset
+    x = apply_transform(x, transform_matrix, fill_mode)
+    return x
+
+
+def zoom(x, zoom_width, zoom_height, fill_mode='nearest'):
+    """Performs a random spatial zoom of a Numpy image tensor.
+
+    # Arguments
+        x: Input tensor. Must be 3D.
+        zoom_range: Tuple of floats; zoom range for width and height.
+        fill_mode: Points outside the boundaries of the input
+            are filled according to the given mode
+            (one of `{'constant', 'nearest', 'reflect', 'wrap'}`).
+
+    # Returns
+        Zoomed Numpy image tensor.
+
+    # Raises
+        ValueError: if `zoom_range` isn't a tuple.
+    """
+    if zoom_width == 1 and zoom_height == 1:
+        zx, zy = 1, 1
+    else:
+        zx, zy = np.random.uniform(zoom_width, zoom_height, 2)
+    zoom_matrix = np.array([[zx, 0, 0],
+                            [0, zy, 0],
+                            [0, 0, 1]])
+
+    h, w = x.shape[0], x.shape[1]
+    transform_matrix = transform_matrix_offset_center(zoom_matrix, h, w)
+    x = apply_transform(x, transform_matrix, fill_mode)
+    return x
+
+
+def shear(x, intensity, fill_mode='nearest'):
+    """Performs a random spatial shear of a Numpy image tensor.
+
+    # Arguments
+        x: Input tensor. Must be 3D.
+        intensity: Transformation intensity.
+            can be negative or positive
+        fill_mode: Points outside the boundaries of the input
+            are filled according to the given mode
+            (one of `{'constant', 'nearest', 'reflect', 'wrap'}`).
+        cval: Value used for points outside the boundaries
+            of the input if `mode='constant'`.
+
+    # Returns
+        Sheared Numpy image tensor.
+    """
+    shear = intensity
+    shear_matrix = np.array([[1, -np.sin(shear), 0],
+                             [0, np.cos(shear), 0],
+                             [0, 0, 1]])
+
+    h, w = x.shape[0], x.shape[1]
+    transform_matrix = transform_matrix_offset_center(shear_matrix, h, w)
+    x = apply_transform(x, transform_matrix, fill_mode)
+    return x
+
+
+def one_hot(y, nb_classes):
+    a = np.zeros((len(y), nb_classes), 'uint8')
+    a[np.arange(len(y)), y] = 1
+    return a
 
 
 def read(path, grayscale=False, crop=None, scale=None, target_size=None,
